f06b491c71c6d82ab23e2862c10d44d8dd63b435
Manucharyand <manucharyand@gmail.com>
2023-12-06 21:55:45 -0700

    fixed redirect for homepage

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 // This app has no "home" page, but your projects should 😀
 router.get('/', function(req, res, next) {
-  // res.redirect('/flights');
+  res.redirect('/flights');
 });
 
 module.exports = router;
